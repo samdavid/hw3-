31127c2392fd0e47ffb09d4d60c7d51bf96f2261
sam david <sam@sampdx.com>
2013-02-15 13:20:27 -0800

    added a random and a guess

--- penny.py
+++ penny.py
@@ -1,5 +1,16 @@
+import random
 roundNum = 0
 
-for i in range(10):
+for i in range(3):
     roundNum += 1
+    compchoice = random.randint(0,15)
+    if compchoice <= 7:
+        guess = "Heads"
+    elif compchoice > 7:
+        guess = "Tails"
     print("Round", roundNum, "start!")
+    print("")
+    print("Player: secretly choose heads or tails and then press <enter>")
+    print("My guess is:", guess, "!")   
+
+    print("")
